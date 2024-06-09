253d4092b31b42b1a53147297ffd3388e50f3017
unknown <vitali.shuman96@gmail.com>
2024-06-09 17:04:57 +0300

    Adds Peashooter

--- src/entities/plants/PeashooterPlant/index.ts
+++ src/entities/plants/PeashooterPlant/index.ts
@@ -1,23 +1,42 @@
-import type { PlantBase } from '@/entities/plants/PlantBase';
+import type { PlantBase, PlantBaseProps } from '@/entities/plants/PlantBase';
 import createPlantBase from '@/entities/plants/PlantBase';
+import createPeaProjectilefrom from '@/entities/projectiles/PeaProjectile';
+import type { PeaProjectile } from '@/entities/projectiles/PeaProjectile';
+import type { ProjectileBase } from '@/entities/projectiles/ProjectileBase';
+import shootFeature from '@/features/shoot';
 
-type PeashooterPlant = PlantBase & {
-  // no additional properties
+type ProjectileBuilder<PR extends ProjectileBase> = ({ damage, x, y }: { damage: number; x: number; y: number }) => PR;
+
+type PeashooterPlant<PR extends ProjectileBase> = PlantBase & {
+  shoot: () => ProjectileBuilder<PR>;
+  getShootRate: () => number;
+  setShootRate: (rate: number) => void;
+  getShootDamage: () => number;
+  setShootDamage: (damage: number) => void;
+  projectile: ProjectileBuilder<PR>;
+  getProjectile: () => ProjectileBuilder<PR>;
+  setProjectile: (projectile: ProjectileBuilder<PR>) => void;
 };
 
-type PeashooterPlantProps = {
-  name: string;
-  health: number;
-  damage: number;
-  attackSpeed: number;
-  x: number;
-  y: number;
+type PeashooterPlantProps = PlantBaseProps & {
+  shootDamage: number;
+  shootRate: number;
 };
 
-const createPeashooterPlant = ({ name, health, damage, attackSpeed, x, y }: PeashooterPlantProps): PeashooterPlant => {
-  const plant = createPlantBase({ name, health, damage, attackSpeed, x, y });
+const createPeashooterPlant = ({ name, health, shootDamage, shootRate, x, y }: PeashooterPlantProps) => {
+  const plant = createPlantBase({ name, health, x, y });
 
-  return plant;
+  const peashooterPlant = {
+    ...shootFeature<PeaProjectile>({
+      shootRate,
+      shootDamage,
+      getShootX: plant.getX,
+      getShootY: plant.getY,
+      projectileBuilder: createPeaProjectilefrom,
+    }),
+  };
+
+  return peashooterPlant;
 };
 
 export default createPeashooterPlant;
